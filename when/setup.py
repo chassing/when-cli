0b091e03680c696c656cd6ba37957cc4d815e744
Christian Assing <chris@ca-net.org>
2022-03-24 14:29:13 +0100

    black

--- when/setup.py
+++ when/setup.py
@@ -1,30 +1,29 @@
-
 import time
 
 from setuptools import setup
 
 setup(
-    name='when-cli',
+    name="when-cli",
     version=int(time.time()),
-    description='WHEN Command Line Interface',
-    author='Christian Assing',
-    author_email='christian.assing@infineon.com',
-    url='https://wiki.intra.infineon.com',
+    description="WHEN Command Line Interface",
+    author="Christian Assing",
+    author_email="chris@ca-net.org",
+    url="https://github.com/chassing/when-cli",
     packages=[
-        'when',
+        "when",
     ],
-    package_dir={'when': 'when'},
+    package_dir={"when": "when"},
     include_package_data=True,
     install_requires=[],
     license="Public Domain (WTFPL)",
-    platforms='any',
-    keywords='when',
+    platforms="any",
+    keywords="when",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 3",
     ],
-    test_suite='tests',
+    test_suite="tests",
 )
